cf72f7bf09fe9a22ad57de2a24229c53bdef661b
feedex20 <fedelazarte2015@gmail.com>
2024-04-15 19:34:38 -0300

    delete comment

--- js/script.js
+++ js/script.js
@@ -5,24 +5,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const nextButton = document.getElementById("nextButton");
   let currentIndex = 0;
 
-  // Función para mostrar la imagen actual
+  
   function showImage(index) {
     const slideWidth = carouselImages[0].clientWidth;
     carouselSlide.style.transform = `translateX(-${index * slideWidth}px)`;
     currentIndex = index;
   }
 
-  // Mostrar la primera imagen al cargar la página
+  
   showImage(currentIndex);
 
-  // Mostrar la imagen anterior al hacer clic en el botón "Anterior"
+ 
   prevButton.addEventListener("click", function () {
     currentIndex =
       currentIndex > 0 ? currentIndex - 1 : carouselImages.length - 1;
     showImage(currentIndex);
   });
 
-  // Mostrar la siguiente imagen al hacer clic en el botón "Siguiente"
+  
   nextButton.addEventListener("click", function () {
     currentIndex =
       currentIndex < carouselImages.length - 1 ? currentIndex + 1 : 0;
